5cd4d08e33cdea78241ff7c6919c262f3cdc8726
Hameemsha H <59426814+hameemsha121@users.noreply.github.com>
2020-03-10 11:55:07 +0530

    Update ArmstrongNumbers.java

--- ArmstrongNumbers.java
+++ ArmstrongNumbers.java
@@ -6,25 +6,7 @@ public class ArmstrongNumbers {
 
 	public static void main(String[] args) {
 	
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the number");
-		int a=sc.nextInt();
-		int r,sum=0;
-		int b=a;
-		while(a!=0)
-		{
-			r=a%10;
-			sum=sum+(r*r*r);
-			a=a/10;
-		}
-		
-		
-		if(sum==b) {
-			System.out.println("armstrong number");
-		}
-		else
-			System.out.println("not armstrong number");
-		
+		System.out.println("Hello World");
 		
 	
 	}
