dc7678eb39523660ba484416acd9667ce21ee3a4
Hameemsha H <59426814+hameemsha121@users.noreply.github.com>
2020-03-10 12:00:22 +0530

    Delete Todaydate.java

--- Todaydate.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-public class TodayDate{
-	public static void main(String[] args){
-	LocalDate date=LocalDate.now();
-	System.out.println("today's date : "+date);
-	LocalTime time=LocalTime.now();
-	System.out.println("current time : "+time);
-}
